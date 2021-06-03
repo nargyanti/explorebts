c5c460bcead1b2fd95495323284fe665d6352f73
Nabilah Argyanti <nargyanti@gmail.com>
2021-06-04 00:58:49 +0700

    Fix layout and UI

--- resources/views/vendor/createProduct.blade.php
+++ resources/views/vendor/createProduct.blade.php
@@ -1,55 +1,8 @@
-<html>
+@extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="shortcut icon" href="{{ asset('assets/img/logo-bts.png') }}" type="image/x-icon">
-    <title>Explore BTS</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">  
-
-    <!-- Custom styles for this template -->
-    <link href="{{ asset('assets/css/shop-homepage.css') }}" rel="stylesheet">
-    <style>
-        .navbar {
-            background-color: #f4623a;
-        }
-
-        td {
-            padding-right: 25px;
-            padding-bottom: 10px;
-        }
-
-        h1 {
-            text-align: center;
-            padding-top: 25px;
-            padding-bottom: 10px;
-        }
-
-        body {
-            background: linear-gradient(to bottom, #f4623a 0%, #343a40 100%);
-            color: white;
-        }
-
-        /* input[type=button],
-        input[type=submit],
-        input[type=reset] {
-            border: 1px;
-            color: #343a40;
-            padding: 16px 32px;
-            text-decoration: none;
-            margin: 4px 2px;
-            cursor: pointer;
-        } */
-    </style>
-</head>
-
-<body>
-    <!-- navbar -->
-    @include('layouts.navigation')
-    <!-- navbar -->
-    <h1>Add Service</h1>
+@section('content')
+    @include('layouts.navigation')        
+    <h1 class="pt-4">Add Service</h1>
     <!--form!-->
     <div class="container">        
         @if ($errors->any())
@@ -65,7 +18,7 @@
         <form action="{{ route('product.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
             <table>
-                <tr>
+                <tr class="my-1">
                     <td>Category</td>
                     <td><select class="form-control" id="category" name="category" required>
                             <option value="Camping Tools">Camping Tools</option>
@@ -75,9 +28,9 @@
                         </select>
                     </td>
                 </tr>
-                <tr>
+                <tr class="my-1">
                     <td>Product Picture</td>
-                    <td><input type="file" name="picture" id="picture" ariadescribedby="Product Picture" required></td>                    
+                    <td><input type="file" name="picture" class="form-control-file" id="picture" ariadescribedby="Product Picture" required></td>                    
                 </tr>
                 <tr>
                     <td>Product Name</td>
@@ -99,12 +52,4 @@
             <button type="submit" class="btn btn-light">Submit</button>
         </form>
     </div>
-    <!-- Footer -->
-    <footer class="py-2 bg-dark">
-        <div class="container">
-            <p class="m-0 text-center text-white">Copyright &copy; 2021 - Advanced Web Programming TI-2H</p>
-        </div>
-    </footer>
-</body>
-
-</html>
+@endsection
