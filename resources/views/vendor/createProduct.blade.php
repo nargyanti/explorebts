12dbfa5898634b146b0f54d8a862b030690dc6d9
Nabilah Argyanti <nargyanti@gmail.com>
2021-06-24 22:39:28 +0700

    Fix product UI

--- resources/views/vendor/createProduct.blade.php
+++ resources/views/vendor/createProduct.blade.php
@@ -1,26 +1,16 @@
 @extends('layouts.app')
-
+@include('layouts.navigation')        
 @section('content')
-    @include('layouts.navigation')        
-    <h1 class="pt-4">Add Service</h1>
+    <h1 class="pt-4">Add Product</h1>
     <!--form!-->
-    <div class="container">        
-        @if ($errors->any())
-        <div class="alert alert-danger">
-            <strong>Whoops!</strong> There were some problems with your input.<br><br>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-        @endif
-        <form action="{{ route('product.store') }}" method="POST" enctype="multipart/form-data">
+    <div class="card mb-3">        
+        @include('layouts.error')
+        <form action="{{ route('product.store') }}" method="POST" enctype="multipart/form-data" class="card-body">
         @csrf
             <table>
                 <tr class="my-1">
                     <td>Category</td>
-                    <td><select class="form-control" id="category" name="category" required>
+                    <td><select class="form-control ml-4" id="category" name="category" required>
                             <option value="Camping Tools">Camping Tools</option>
                             <option value="Inn">Inn</option>
                             <option value="Jeep">Jeep</option>
@@ -30,26 +20,26 @@
                 </tr>
                 <tr class="my-1">
                     <td>Product Picture</td>
-                    <td><input type="file" name="picture" class="form-control-file" id="picture" ariadescribedby="Product Picture" required></td>                    
+                    <td><input type="file" name="picture" class="form-control-file ml-4" id="picture" ariadescribedby="Product Picture" required></td>                    
                 </tr>
                 <tr>
                     <td>Product Name</td>
-                    <td><input type="text" name="name" class="form-control" id="name" aria-describedby="name" placeholder="Product Name" required></td>
+                    <td><input type="text" name="name" class="form-control ml-4" id="name" aria-describedby="name" placeholder="Product Name" required></td>
                 </tr>
                 <tr>
                     <td>Unit Price</td>                    
-                    <td><input type="number" name="unit_price" class="form-control" id="unit_price" aria-describedby="unit_price" placeholder="Unit Price" required></td>
+                    <td><input type="number" name="unit_price" class="form-control ml-4" id="unit_price" aria-describedby="unit_price" placeholder="Unit Price" required></td>
                 </tr>
                 <tr>
                     <td>Product Stock</td>                    
-                    <td><input type="number" name="stock" class="form-control" id="stock" aria-describedby="stock" placeholder="Product Stock" required></td>
+                    <td><input type="number" name="stock" class="form-control ml-4" id="stock" aria-describedby="stock" placeholder="Product Stock" required></td>
                 </tr>
                 <tr>
                     <td>Product Description</td>                    
-                    <td><textarea name="description" id="description" class="form-control" aria-describedby="description" placeholder="Product Description" cols="10" rows="5" required></textarea></td>
+                    <td><textarea name="description" id="description" class="form-control ml-4" aria-describedby="description" placeholder="Product Description" cols="10" rows="5" required></textarea></td>
                 </tr>
             </table>
-            <button type="submit" class="btn btn-light">Submit</button>
+            <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
 @endsection
--- resources/views/vendor/updateProduct.blade.php
+++ resources/views/vendor/updateProduct.blade.php
@@ -1,25 +1,16 @@
 @extends('layouts.app')
-
+@include('layouts.navigation')        
 @section('content')
-    @include('layouts.navigation')
-    <h1>Update Product</h1>            
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        <strong>Whoops!</strong> There were some problems with your input.<br><br>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-    @endif
-    <form action="{{ route('product.update', $product->id) }}" method="POST" enctype="multipart/form-data">
+<h1 class="pt-4">Edit Product</h1>        
+@include('layouts.error')
+<div class="card mb-4">
+    <form action="{{ route('product.update', $product->id) }}" method="POST" enctype="multipart/form-data" class="card-body">
         @csrf
         @method('PUT')
         <table>
             <tr>
                 <td>Category</td>
-                <td><select class="form-control" id="category" name="category" required>
+                <td><select class="form-control ml-4" id="category" name="category" required>
                         <option value="Camping Tools" {{ $product->category == "Camping Tools" ? 'selected' : '' }}>Camping Tools</option>
                         <option value="Inn" {{ $product->category == "Inn" ? 'selected' : '' }}>Inn</option>
                         <option value="Jeep" {{ $product->category == "Jeep" ? 'selected' : '' }}>Jeep</option>
@@ -29,25 +20,26 @@
             </tr>
             <tr>
                 <td>Product Picture</td>
-                <td><input type="file" name="picture" class="form-control-file" id="picture" ariadescribedby="Product Picture" value="{{ $product->picture }}"></td>                    
+                <td><input type="file" name="picture" class="form-control-file ml-4" id="picture" ariadescribedby="Product Picture" value="{{ $product->picture }}"></td>                    
             </tr>
             <tr>
                 <td>Product Name</td>
-                <td><input type="text" name="name" class="form-control" id="name" aria-describedby="name" placeholder="Product Name" value="{{ $product->name }}" required></td>
+                <td><input type="text" name="name" class="form-control ml-4" id="name" aria-describedby="name" placeholder="Product Name" value="{{ $product->name }}" required></td>
             </tr>
             <tr>
                 <td>Unit Price</td>                    
-                <td><input type="number" name="unit_price" class="form-control" id="unit_price" aria-describedby="unit_price" placeholder="Unit Price" value="{{ $product->unit_price }}" required></td>
+                <td><input type="number" name="unit_price" class="form-control ml-4" id="unit_price" aria-describedby="unit_price" placeholder="Unit Price" value="{{ $product->unit_price }}" required></td>
             </tr>
             <tr>
                 <td>Product Stock</td>                    
-                <td><input type="number" name="stock" class="form-control" id="stock" aria-describedby="stock" placeholder="Product Stock" value="{{ $product->stock }}" required></td>
+                <td><input type="number" name="stock" class="form-control ml-4" id="stock" aria-describedby="stock" placeholder="Product Stock" value="{{ $product->stock }}" required></td>
             </tr>
             <tr>
                 <td>Product Description</td>                    
-                <td><textarea name="description" id="description" class="form-control" aria-describedby="description" placeholder="Product Description" cols="10" rows="5" required>{{ $product->description }}</textarea></td>
+                <td><textarea name="description" id="description" class="form-control ml-4" aria-describedby="description" placeholder="Product Description" cols="10" rows="5" required>{{ $product->description }}</textarea></td>
             </tr>
         </table>
-        <button type="submit" class="btn btn-light">Submit</button>
+        <button type="submit" class="btn btn-primary">Submit</button>
     </form>
+    </div>
 @endsection
